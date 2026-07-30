105eff8d8b5a3e3e996e5343f58aea2d79d4b996
Ryan Scarbery <oliver42@fib896.com>
2026-07-29 20:39:21 -0700

    .

--- src/lib/state.js
+++ src/lib/state.js
@@ -3,5 +3,5 @@ import { writable } from 'svelte/store';
 export const cur = writable(7);
 
 export function goTo(idx) {
-  cur.update(c => (idx < 0 || idx > 8 || idx === c) ? c : idx);
+  cur.update(c => (idx < 0 || idx > 9 || idx === c) ? c : idx);
 }
